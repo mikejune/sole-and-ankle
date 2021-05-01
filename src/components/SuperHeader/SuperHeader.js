b6d9ab7a7444b86dc763cfe535ef6f6d42fea0d4
Michael Junio <michael.ray.junio@gmail.com>
2021-05-01 16:45:52 +1000

    Superheader alternative solutions

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -6,6 +6,7 @@ import { COLORS } from '../../constants';
 import SearchInput from '../SearchInput';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
+// import Spacer from '../Spacer' // Not-so-modern solution #2
 
 const SuperHeader = () => {
   return (
@@ -14,7 +15,9 @@ const SuperHeader = () => {
         Free shipping on domestic orders over $75!
       </MarketingMessage>
       <SearchInput />
+      {/* <Spacer size={24} /> */}
       <HelpLink href="/help">Help</HelpLink>
+      {/* <Spacer size={24} /> */}
       <UnstyledButton>
         <Icon id="shopping-bag" strokeWidth={1} />
       </UnstyledButton>
@@ -26,10 +29,22 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
-  display: flex;
-  gap: 24px;
   align-items: center;
+
+  /* Solution */
+  display: flex;
+
+  /* Modern Solution */
+  gap: 24px;
+
+  /* My Solution */
   padding: 8px 32px;
+
+  /* JC Solution */
+  height: 40px; 
+  padding-right: 32px;
+  padding-left: 32px;
+
 `;
 
 const MarketingMessage = styled.span`
@@ -41,6 +56,10 @@ const HelpLink = styled.a`
   color: inherit;
   text-decoration: none;
   outline-offset: 2px;
+
+  /* Not-so-modern Solution #1 */
+  /* margin-left: 24px; */
+  /* margin-right: 24px; */
 
   &:not(:focus-visible) {
     outline: none;
